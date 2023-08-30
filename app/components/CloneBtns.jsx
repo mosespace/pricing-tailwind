e637e29db13799c33a5d8639ac64a98159b82ae0
mosespace <kisakyemoses50@gmail.com>
2023-08-30 10:20:04 +0300

    feat: updated styles

--- app/components/CloneBtns.jsx
+++ app/components/CloneBtns.jsx
@@ -8,15 +8,13 @@ export default function CloneBtns() {
     setBtnClicked(btnClicked);
   }
   return (
-    <section className='py-2 px-[1rem]'>
+    <section className='py-2 px-[2rem]'>
       <div id='#buttons' className='bg-slate-900 py-4 rounded-3xl'>
         <div className='px-4 gap-6 flex flex-wrap'>
           <button
             onClick={() => handleBtnClick("all")}
             className={`${
-              btnClicked === "all"
-                ? "bg-blue-800 px-8 py-2 rounded-3xl"
-                : ""
+              btnClicked === "all" ? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
             }`}
           >
             All
@@ -24,31 +22,59 @@ export default function CloneBtns() {
           <button
             onClick={() => handleBtnClick("agency")}
             className={`${
-              btnClicked === "agency"? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
+              btnClicked === "agency" ? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
             }`}
           >
             Agency
           </button>
-          <button onClick={() => handleBtnClick("business")}
+          <button
+            onClick={() => handleBtnClick("business")}
             className={`${
-              btnClicked === "business"? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
-            }`}>Business</button>
-          <button onClick={() => handleBtnClick("saas")}
+              btnClicked === "business"
+                ? "bg-blue-800 px-8 py-2 rounded-3xl"
+                : ""
+            }`}
+          >
+            Business
+          </button>
+          <button
+            onClick={() => handleBtnClick("saas")}
             className={`${
-              btnClicked === "saas"? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
-            }`}>SaaS</button>
-          <button onClick={() => handleBtnClick("startup")}
+              btnClicked === "saas" ? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
+            }`}
+          >
+            SaaS
+          </button>
+          <button
+            onClick={() => handleBtnClick("startup")}
             className={`${
-              btnClicked === "startup"? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
-            }`}>Startup</button>
-          <button onClick={() => handleBtnClick("e-commerce")}
+              btnClicked === "startup"
+                ? "bg-blue-800 px-8 py-2 rounded-3xl"
+                : ""
+            }`}
+          >
+            Startup
+          </button>
+          <button
+            onClick={() => handleBtnClick("e-commerce")}
             className={`${
-              btnClicked === "e-commerce"? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
-            }`}>ECommerce</button>
-          <button onClick={() => handleBtnClick("dashboard")}
+              btnClicked === "e-commerce"
+                ? "bg-blue-800 px-8 py-2 rounded-3xl"
+                : ""
+            }`}
+          >
+            ECommerce
+          </button>
+          <button
+            onClick={() => handleBtnClick("dashboard")}
             className={`${
-              btnClicked === "dashboard"? "bg-blue-800 px-8 py-2 rounded-3xl" : ""
-            }`}>Dashboard</button>
+              btnClicked === "dashboard"
+                ? "bg-blue-800 px-8 py-2 rounded-3xl"
+                : ""
+            }`}
+          >
+            Dashboard
+          </button>
         </div>
       </div>
     </section>
